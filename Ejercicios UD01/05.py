bde6edbe48c819634b7719cabcd166209499a421
Lucas Flores <lucasfloresben@gmail.com>
2025-11-07 19:42:34 +0100

    Prueba

---
+++ Ejercicios UD01/05.py
@@ -0,0 +1,13 @@
+import math
+
+radio = int(input("Introduzca el radio de la esfera: "))
+
+diametro = radio * 2
+
+longitud = 2 * math.pi * radio
+area = math.pi * radio**2
+volumen = 4/3 * math.pi * radio**3
+
+print(longitud)
+print(area)
+print(volumen)
