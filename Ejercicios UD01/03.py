ba588d1e6f8d86b596f1c61b3f4d0435aa880c5c
Lucas Flores <lucasfloresben@gmail.com>
2025-11-07 19:38:11 +0100

    first commit

---
+++ Ejercicios UD01/03.py
@@ -0,0 +1,5 @@
+#Dibuja un ordinograma que calcule el área de un cuadrado cuyo lado se introduce por
+#teclado (El área del cuadrado es igual a lado por lado).
+lado = float(input("Introduce la longitud del lado del cuadrado: "))
+area = lado * lado
+print("El área del cuadrado es: ", area)
---
+++ Ejercicios UD01/04.py
@@ -0,0 +1,10 @@
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+suma = num1 + num2
+resta = num1 - num2
+producto = num1 * num2
+division = num1 / num2
+print(f"La suma de ambos números es: {suma}")
+print(f"La resta de ambos números es: {resta}")
+print(f"La multiplicación de ambos números es: {producto}")
+print(f"La división de ambos números es: {division}")
